f867d650c4587717c34d5166451f74b6620d95e0
Ardyezapratama <ardyezapratama@gmail.com>
2025-03-14 08:44:16 +0800

    Creating Class Workout, Class Child Running and Class Child Cycling

--- app.js
+++ app.js
@@ -11,6 +11,48 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
+class Workout {
+	date = new Date();
+	id = Date.now().toString().slice(-10);
+	constructor(coords, distance, duration) {
+		this.coords = coords; //[lat, lng]
+		this.distance = distance; // in km
+		this.duration = duration; // in min
+	}
+}
+
+class Running extends Workout {
+	constructor(coords, distance, duration, cadence) {
+		super(coords, distance, duration);
+		this.cadence = cadence;
+		this.calcPace();
+	}
+
+	calcPace() {
+		// min/km
+		this.pace = this.duration / this.distance;
+		return this.pace;
+	}
+}
+class Cycling extends Workout {
+	constructor(coords, distance, duration, elevationGain) {
+		super(coords, distance, duration);
+		this.elevationGain = elevationGain;
+		this.calcSpeed();
+	}
+
+	calcSpeed() {
+		// Km / h
+		this.speed = this.distance / (this.duration / 60);
+		return this.speed;
+	}
+}
+
+const run1 = new Running([-8.7870211, 115.1745831], 5.2, 24, 178);
+const cycling1 = new Cycling([-8.7870211, 115.1745831], 27, 95, 523);
+console.log(run1, cycling1);
+
+// Application Class
 class App {
 	#map;
 	#mapEvt;
